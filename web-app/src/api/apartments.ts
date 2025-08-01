4b669e7095b20ee756c849db80c29aa2eecf0b03
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2025-08-01 05:30:25 +0300

    api handlers

--- web-app/src/api/apartments.ts
+++ web-app/src/api/apartments.ts
@@ -7,3 +7,34 @@ export async function getApartments() {
   const apartments: Apartment[] = await res.json();
   return apartments;
 }
+
+export async function getApartment(id: Apartment["id"]) {
+  const res = await fetch(`${baseUrl}/apartments/${id}`);
+  const apartment: Apartment = await res.json();
+  return apartment;
+}
+
+export async function createApartment(apartment: Apartment) {
+  const res = await fetch(`${baseUrl}/apartments`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(apartment),
+  });
+  const newApartment: Apartment = await res.json();
+  return newApartment;
+}
+
+export async function updateApartment(apartment: Apartment) {
+  const res = await fetch(`${baseUrl}/apartments/${apartment.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(apartment),
+  });
+  const updatedApartment: Apartment = await res.json();
+  return updatedApartment;
+}
+
+export async function deleteApartment(id: Apartment["id"]) {
+  const res = await fetch(`${baseUrl}/apartments/${id}`, { method: "DELETE" });
+  return res.json();
+}
