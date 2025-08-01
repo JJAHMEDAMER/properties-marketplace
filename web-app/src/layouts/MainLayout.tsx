3335c3ab5e4d8629892ae5330d3b74f4c25282ec
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2025-08-01 04:03:53 +0300

    header

---
+++ web-app/src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/shared/Header";
+import React from "react";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+}
---
+++ web-app/src/shared/ThemeToggle.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import { Sun, Moon } from "lucide-react";
+
+export default function ThemeToggle() {
+  const [darkMode, setDarkMode] = React.useState(
+    localStorage.getItem("theme") === "dark"
+  );
+
+  React.useEffect(() => {
+    document.documentElement.classList.toggle("dark");
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+  }, [darkMode]);
+
+  return (
+    <button
+      onClick={() => setDarkMode(!darkMode)}
+      className="ml-4 p-2 rounded-full cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 transition"
+    >
+      {darkMode ? <Sun size={18} /> : <Moon size={18} />}
+    </button>
+  );
+}
