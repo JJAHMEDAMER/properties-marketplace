340ef3c622a0c03a07b7a91be6bff7e45febf400
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2025-08-02 13:19:12 +0300

    update image path

--- server/src/middlewares/multer.middleware.ts
+++ server/src/middlewares/multer.middleware.ts
@@ -1,7 +1,9 @@
 import multer from "multer";
 import fs from "fs";
+import { config } from "../config/base";
 
-const uploadDir = "./src/uploads";
+const isProduction = config.nodeEnv === "production";
+const uploadDir = isProduction ? "./dist/src/uploads" : "./src/uploads";
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
