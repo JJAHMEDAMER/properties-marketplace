b1382fb6251bbb795d8df4300dd092aa9972e1da
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2025-08-02 10:14:45 +0300

    create file folder if it doesn't exist

--- server/src/middlewares/multer.middleware.ts
+++ server/src/middlewares/multer.middleware.ts
@@ -1,9 +1,15 @@
-import { NextFunction } from "express";
 import multer from "multer";
+import fs from "fs";
+
+const uploadDir = "./src/uploads";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./src/uploads");
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
